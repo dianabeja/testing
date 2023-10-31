4dcbc68509936f8bdaf18eb992e4384e4825fe11
Diana Bautista <bagdiana03@gmail.com>
2023-10-31 11:18:57 -0600

    cambios finales

---
+++ src/app/simpson/simpson.component.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SimpsonComponent } from './simpson.component';
+
+describe('SimpsonComponent', () => {
+  let component: SimpsonComponent;
+  let fixture: ComponentFixture<SimpsonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SimpsonComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SimpsonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  
+  it('Should return p=16.0 if x0=0, x1=4, num_seg=4, ERROR=0.0001 and f(x)=2x', () => {
+    const resultado = component.simpson("2x",0, 4, 4, 0.0001);
+    expect(resultado).toBe(16);
+  });
+
+  it('Should return p=0.3333 if x0=0, x1=1, num_seg=4, ERROR=0.0001 and f(x)=x^2', () => {
+    const resultado = component.simpson("x*x",0, 1, 4, 0.0001);
+    expect(resultado).toBe(0.33333);
+  });
+
+  it('Should return p=1.38 if x0=1, x1=4, num_seg=6, ERROR=0.001 and f(x)=1/x', () => {
+    const resultado = component.simpson("1/x",1, 1, 6, 0.0001);
+    expect(resultado).toBe(1);
+  });
+});
