ea76427c7b7461651a9185dba3234ef189531c64
Diana Bautista <bagdiana03@gmail.com>
2023-11-14 11:57:54 -0600

    cambios menu

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -19,59 +19,45 @@ export class MediaComponent implements OnInit {
     public dataServiceHours: DataService2
   ) {}
 
+  public array_elegido: any;
+  public resultado = 0;
+
   async ngOnInit(): Promise<void> {
     await this.getHours();
     await this.getSize();
     this.obtenerMediaHours();
-    this.obtenerMediaSize();
   }
 
   async getHours() {
     return new Promise<void>((resolve, reject) => {
-      this.dataServiceHours.getMedia().subscribe(
-        (data: any) => {
-          this.numbers_hours = data.data;
-          resolve();
-        },
-        (error) => {
-          console.error('Error al obtener los datos de horas:', error);
-          reject(error);
-        }
-      );
+      this.dataServiceHours.getMedia().subscribe((data: any[]) => {
+        this.numbers_hours = data;
+        this.array_elegido = data;
+        resolve();
+      });
     });
   }
 
   async getSize() {
     return new Promise<void>((resolve, reject) => {
-      this.dataServiceSize.getSize().subscribe(
-        (data: any) => {
-          this.numbers_size = data.data;
-          resolve();
-        },
-        (error) => {
-          console.error('Error al obtener los datos de size:', error);
-          reject(error);
-        }
-      );
+      this.dataServiceSize.getSize().subscribe((data: any[]) => {
+        this.numbers_hours = data;
+        this.array_elegido = data;
+        resolve();
+      });
     });
   }
 
-  async obtenerMediaSize() {
-    if (this.numbers_size && this.numbers_size && Array.isArray(this.numbers_size)) {
-      this.media_size = this.getMedia(...this.numbers_size);
-    } else {
-      console.log('No hay datos disponibles para calcular la media de size.');
+  async obtenerMediaHours() {
+    if (this.numbers_hours && this.numbers_hours.data && Array.isArray(this.numbers_hours.data)) {
+      this.media_hours = this.getMedia(...this.numbers_hours.data);
     }
   }
 
-  async obtenerMediaHours() {
-    if (this.numbers_hours && this.numbers_hours && Array.isArray(this.numbers_hours)) {
-      this.media_hours = this.getMedia(...this.numbers_hours);
-      console.log(this.media_hours)
-    } else {
-      console.log('No hay datos disponibles para calcular la media de horas.');
+  async obtenerMediaSize() {
+    if (this.numbers_size && this.numbers_size.data && Array.isArray(this.numbers_size.data)) {
+      this.media_size = this.getMedia(...this.numbers_size.data);
     }
-    return this.media_hours;
   }
 
   getMedia(...numbers: number[]): number {
@@ -79,9 +65,14 @@ export class MediaComponent implements OnInit {
     const sum = numbers.reduce((a, b) => a + b, 0);
     return +(sum / numbers.length).toFixed(2);
   }
-}
 
-export function getMedia(...numbers: number[]): number {
+  calcularMedia(array: number[] | any) {
+    const result = this.getMedia(...array.data);
+    this.resultado = result;
+    return result;
+  }
+  }
+  export function getMedia(...numbers: number[]): number {
   if (numbers.length === 0) return 0;
   const sum = numbers.reduce((a, b) => a + b, 0);
   return +(sum / numbers.length).toFixed(2);
